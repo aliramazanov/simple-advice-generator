f300c225b931f1d02989eeb9119d620f2564d0dd
Ali Ramazanov <aliasifzade@gmail.com>
2023-08-19 23:18:35 +0400

    Added a button & changed logic of lifecycle method

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,53 @@ import "./App.css";
 import axios from "axios";
 
 class App extends Component {
-  state = { advice: "" };
+  state = { advice: "", loading: false };
 
   componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.advice === this.state.advice && !this.state.loading) {
+      this.fetchAdvice();
+    }
+  }
+
+  fetchAdvice = () => {
+    this.setState({ loading: true });
+
     axios
       .get("https://api.adviceslip.com/advice")
       .then((response) => {
         const { advice } = response.data.slip;
-        this.setState({ advice });
+        this.setState({ advice, loading: false });
       })
       .catch((error) => {
-        console.log("Error", error);
+        console.log("Error fetching advice:", error);
+        this.setState({ loading: false });
       });
-  }
+  };
+
+  handleButtonClick = () => {
+    if (!this.state.loading) {
+      this.fetchAdvice();
+    }
+  };
 
   render() {
-    const { advice } = this.state;
-    return <h1>{advice}</h1>;
+    const { advice, loading } = this.state;
+    return (
+      <div className="App">
+        <h1 className="content">{advice}</h1>
+        <button
+          className="button-to-fetch"
+          onClick={this.handleButtonClick}
+          disabled={loading}
+        >
+          {loading ? "Advices are on the way..." : "Give me a new advice!"}
+        </button>
+      </div>
+    );
   }
 }
 
